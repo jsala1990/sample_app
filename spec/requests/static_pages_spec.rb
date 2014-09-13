6f93c8b166e48f2da056bd29fa8a5b753988ac38
Jesus Salazar <jsalazar@thoughtworks.com>
2014-09-13 18:07:54 -0500

    ADding contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,42 +4,54 @@ describe "StaticPages" do
 
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Ror Tutorial Sample App')
     end
 
     it "should not have a customt title page" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to_not have_title('| Home')
     end
   end
 
   describe "Help Page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Ror Tutorial Sample App | Help')
     end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('Ror Tutorial Sample App | About Us')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title('Ror Tutorial Sample App | Contact')
     end
   end
 end
