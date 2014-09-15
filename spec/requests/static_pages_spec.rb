f80e2236815cfb311ec762f09cdd3f79ad59932c
Jesus Salazar <jsalazar@thoughtworks.com>
2014-09-14 20:12:27 -0500

    refactoring tests, still need to unit test them because of rspec of plane mistake

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,28 +4,59 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home Page" do
     before(:each) { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help Page" do
     before(:each) { visit help_path }
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before(:each) { visit about_path }
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Page" do
     before(:each) { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe 'Static pages' do
+    it 'should have the right links on the layout' do
+      visit root_path
+      click_link "About"
+      should have_title(full_title('About Us'))
+      click_link "Help"
+      should have_title(full_title('Help'))
+      click_link "Contact"
+      should have_title(full_title('Contact'))
+      click_link "Home"
+      should have_title(full_title(''))
+      click_link "Sign up now!"
+      should have_title(full_title('Sign up'))
+      click_link "sample app"
+      should have_title(full_title(''))
+    end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title page_title
-  base_title = 'Ror Tutorial Sample App'
-  if page_title.empty? then
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
